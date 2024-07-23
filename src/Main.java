cf8ffd99a790260ff6e8f348172207aa4300b1be
k0k <biet00biet@gmail.com>
2024-07-23 09:16:48 +0700

    solved addTwoNumbers

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(18/10);
         Solution solution = new Solution();
         solution.twoSum(new int[]{2,7,11,15}, 9);
     }
--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,42 @@
 import java.util.HashMap;
 import java.util.Map;
 
+
+
 public class Solution {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode tmp1 = l1;
+        ListNode tmp2 = l2;
+
+        ListNode result = new ListNode();
+        ListNode tmp = null;
+
+        int carry = 0;
+
+        while (tmp1 != null || tmp2 != null || carry != 0) {
+            int val1 = tmp1 != null ? tmp1.val : 0;
+            int val2 = tmp2 != null ? tmp2.val : 0;
+
+            int sum = val1 + val2 + carry;
+
+            carry = sum / 10;
+
+            tmp1 = tmp1 != null ? tmp1.next : null;
+            tmp2 = tmp2 != null ? tmp2.next: null;
+
+            if(tmp == null) {
+                tmp = result;
+            } else {
+                tmp.next = new ListNode();
+                tmp = tmp.next;
+            }
+
+            tmp.val = sum - 10*carry;
+
+        }
+        return result;
+    }
     public int[] twoSum(int[] nums, int target) {
 
         Map<Integer,Integer> traveled = new HashMap<>();
